eb8b1658810c4497b68a100c0aa43de3e2a43186
Steven Scott <steven.scott@aggiemail.usu.edu>
2018-11-16 19:17:36 -0700

    Fixed trailing slashes in paths

--- Web_App_Template.py
+++ Web_App_Template.py
@@ -5,23 +5,23 @@ app= Flask(__name__)
 def hello():
 	return "<h1>Home Page!</h1>" 
 
-@app.route("/about")
+@app.route("/about/")
 def about_func():
 	return "<h1>This is the about page!</h1>" 
 
-@app.route("/anxiety_assessment")
+@app.route("/anxiety_assessment/")
 def anxiety_assessment():
     return "<h1>Anxiety assessment page</h1>"
 
-@app.route("/anxiety_assessment/results")
+@app.route("/anxiety_assessment/results/")
 def anxiety_assessment_results():
     return "<h1>Anxiety assessment results page</h1>"
 
-@app.route("/depression_assessment")
+@app.route("/depression_assessment/")
 def depression_assessment():
     return "<h1>Depression assessment page</h1>"
 
-@app.route("/depression_assessment/results")
+@app.route("/depression_assessment/results/")
 def depression_assessment_results():
     return "<h1>Depression assessment results page</h1>"
 
