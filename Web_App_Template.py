9b5ccb5ff44e1e9406abaa2d3e15a3c2b5b6236f
m <m@ms-MacBook-Pro.local>
2018-11-16 20:03:57 -0700

    updates

--- Web_App_Template.py
+++ Web_App_Template.py
@@ -7,16 +7,15 @@ def hello():
 
 @app.route("/AnxietyExam")
 def E_Exam():
-	return render_template('test.html')
-	
+	return render_template('Anxiety_Exam.html')	
 
 @app.route("/DepressionExam")
 def D_Exam():
-	return "<h1>This is the about page!</h1>" 
+	return render_template('Depression_Exam.html')	 
 
 @app.route("/Results_A")
 def results_1():
-	return "<h1>This is the about page!</h1>" 
+	return render_template('result')
 
 @app.route("/Results_D")
 def results_2():
